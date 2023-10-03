1a1d15813fc0a49e92af5974b3ca6d72ebb23b0b
korayerbas <115890098+korayerbas@users.noreply.github.com>
2023-10-03 21:40:04 +0300

    RGBuvHistBlock.py

--- RGBuvHistBlock.py
+++ RGBuvHistBlock.py
@@ -1,66 +1,33 @@
 
-"""
-Created on Sun Sep 17 17:54:02 2023
-
-@author: Koray Uğur Erbaş
-"""
-
-"""
-##### Copyright 2021 Mahmoud Afifi.
-
- If you find this code useful, please cite our paper:
-
- Mahmoud Afifi, Marcus A. Brubaker, and Michael S. Brown. "HistoGAN:
- Controlling Colors of GAN-Generated and Real Images via Color Histograms."
- In CVPR, 2021.
-
- @inproceedings{afifi2021histogan,
-  title={Histo{GAN}: Controlling Colors of {GAN}-Generated and Real Images via
-  Color Histograms},
-  author={Afifi, Mahmoud and Brubaker, Marcus A. and Brown, Michael S.},
-  booktitle={CVPR},
-  year={2021}
-}
-####
-"""
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
+##### Copyright 2021 Mahmoud Afifi.######
 
 EPS = 1e-6
-
-
 class RGBuvHistBlock(nn.Module):
   def __init__(self, h=64, insz=150, resizing='interpolation',
                method='inverse-quadratic', sigma=0.02, intensity_scale=True,
-               hist_boundary=None, green_only=False, device='cuda'):
-    """ Computes the RGB-uv histogram feature of a given image.
-    Args:
-      h: histogram dimension size (scalar). The default value is 64.
-      insz: maximum size of the input image; if it is larger than this size, the
-        image will be resized (scalar). Default value is 150 (i.e., 150 x 150
-        pixels).
-      resizing: resizing method if applicable. Options are: 'interpolation' or
-        'sampling'. Default is 'interpolation'.
-      method: the method used to count the number of pixels for each bin in the
-        histogram feature. Options are: 'thresholding', 'RBF' (radial basis
-        function), or 'inverse-quadratic'. Default value is 'inverse-quadratic'.
-      sigma: if the method value is 'RBF' or 'inverse-quadratic', then this is
-        the sigma parameter of the kernel function. The default value is 0.02.
-      intensity_scale: boolean variable to use the intensity scale (I_y in
-        Equation 2). Default value is True.
-      hist_boundary: a list of histogram boundary values. Default is [-3, 3].
-      green_only: boolean variable to use only the log(g/r), log(g/b) channels.
-        Default is False.
+               device='cuda')
+ # """ Computes the RGB-uv histogram feature of a given image.
+ #   Args:
+ #     h: histogram dimension size (scalar). The default value is 64.
+ #     insz: maximum size of the input image; if it is larger than this size, the 
+ #       image will be resized (scalar). Default value is 150 (i.e., 150 x 150 
+ #       pixels).
+ #    resizing: resizing method if applicable. Options are: 'interpolation' or 
+ #      'sampling'. Default is 'interpolation'.
+ #    method: the method used to count the number of pixels for each bin in the 
+ #      histogram feature. Options are: 'thresholding', 'RBF' (radial basis 
+ #      function), or 'inverse-quadratic'. Default value is 'inverse-quadratic'.
+ #    sigma: if the method value is 'RBF' or 'inverse-quadratic', then this is 
+ #      the sigma parameter of the kernel function. The default value is 0.02.
+ #    intensity_scale: boolean variable to use the intensity scale (I_y in 
+ #      Equation 2). Default value is True.
 
-    Methods:
-      forward: accepts input image and returns its histogram feature. Note that
-        unless the method is 'thresholding', this is a differentiable function
-        and can be easily integrated with the loss function. As mentioned in the
-         paper, the 'inverse-quadratic' was found more stable than 'RBF' in our
-         training.
+ #   Methods:
+ #     forward: accepts input image and returns its histogram feature. Note that 
+ #     unless the method is 'thresholding', this is a differentiable function 
+ #     and can be easily integrated with the loss function. As mentioned in the
+ #     paper, the 'inverse-quadratic' was found more stable than 'RBF' in our 
+ #     training.
     """
     super(RGBuvHistBlock, self).__init__()
     self.h = h
@@ -69,13 +36,8 @@ class RGBuvHistBlock(nn.Module):
     self.resizing = resizing
     self.method = method
     self.intensity_scale = intensity_scale
-    self.green_only = green_only 
-    if hist_boundary is None:
-      hist_boundary = [-3, 3]
-    hist_boundary.sort()
-    self.hist_boundary = hist_boundary
     if self.method == 'thresholding':
-      self.eps = (abs(hist_boundary[0]) + abs(hist_boundary[1])) / h
+      self.eps = 6.0 / h
     else:
       self.sigma = sigma
 
@@ -105,67 +67,63 @@ class RGBuvHistBlock(nn.Module):
     if x_sampled.shape[1] > 3:
       x_sampled = x_sampled[:, :3, :, :]
     X = torch.unbind(x_sampled, dim=0)
-    hists = torch.zeros((x_sampled.shape[0], 1 + int(not self.green_only) * 2,
-                         self.h, self.h)).to(device=self.device)
+    hists = torch.zeros((x_sampled.shape[0], 3, self.h, self.h)).to(
+      device=self.device)
     for l in range(L):
       I = torch.t(torch.reshape(X[l], (3, -1)))
       II = torch.pow(I, 2)
       if self.intensity_scale:
-        Iy = torch.unsqueeze(torch.sqrt(II[:, 0] + II[:, 1] + II[:, 2] + EPS),
+        Iy = torch.unsqueeze(torch.sqrt(II[:, 0] + II[:, 1] + II[:, 2] + EPS), 
                              dim=1)
       else:
         Iy = 1
-      if not self.green_only:
-        Iu0 = torch.unsqueeze(torch.log(I[:, 0] + EPS) - torch.log(I[:, 1] +
-                                                                   EPS), dim=1)
-        Iv0 = torch.unsqueeze(torch.log(I[:, 0] + EPS) - torch.log(I[:, 2] +
-                                                                   EPS), dim=1)
-        diff_u0 = abs(
-          Iu0 - torch.unsqueeze(torch.tensor(np.linspace(
-            self.hist_boundary[0], self.hist_boundary[1], num=self.h)),
-            dim=0).to(self.device))
-        diff_v0 = abs(
-          Iv0 - torch.unsqueeze(torch.tensor(np.linspace(
-            self.hist_boundary[0], self.hist_boundary[1], num=self.h)),
-            dim=0).to(self.device))
-        if self.method == 'thresholding':
-          diff_u0 = torch.reshape(diff_u0, (-1, self.h)) <= self.eps / 2
-          diff_v0 = torch.reshape(diff_v0, (-1, self.h)) <= self.eps / 2
-        elif self.method == 'RBF':
-          diff_u0 = torch.pow(torch.reshape(diff_u0, (-1, self.h)),
-                              2) / self.sigma ** 2
-          diff_v0 = torch.pow(torch.reshape(diff_v0, (-1, self.h)),
-                              2) / self.sigma ** 2
-          diff_u0 = torch.exp(-diff_u0)  # Radial basis function
-          diff_v0 = torch.exp(-diff_v0)
-        elif self.method == 'inverse-quadratic':
-          diff_u0 = torch.pow(torch.reshape(diff_u0, (-1, self.h)),
-                              2) / self.sigma ** 2
-          diff_v0 = torch.pow(torch.reshape(diff_v0, (-1, self.h)),
-                              2) / self.sigma ** 2
-          diff_u0 = 1 / (1 + diff_u0)  # Inverse quadratic
-          diff_v0 = 1 / (1 + diff_v0)
-        else:
-          raise Exception(
-            f'Wrong kernel method. It should be either thresholding, RBF,'
-            f' inverse-quadratic. But the given value is {self.method}.')
-        diff_u0 = diff_u0.type(torch.float32)
-        diff_v0 = diff_v0.type(torch.float32)
-        a = torch.t(Iy * diff_u0)
-        hists[l, 0, :, :] = torch.mm(a, diff_v0)
+
+      Iu0 = torch.unsqueeze(torch.log(I[:, 0] + EPS) - torch.log(I[:, 1] + EPS),
+                            dim=1)
+      Iv0 = torch.unsqueeze(torch.log(I[:, 0] + EPS) - torch.log(I[:, 2] + EPS),
+                            dim=1)
+      diff_u0 = abs(
+        Iu0 - torch.unsqueeze(torch.tensor(np.linspace(-3, 3, num=self.h)),
+                              dim=0).to(self.device))
+      diff_v0 = abs(
+        Iv0 - torch.unsqueeze(torch.tensor(np.linspace(-3, 3, num=self.h)),
+                              dim=0).to(self.device))
+      if self.method == 'thresholding':
+        diff_u0 = torch.reshape(diff_u0, (-1, self.h)) <= self.eps / 2
+        diff_v0 = torch.reshape(diff_v0, (-1, self.h)) <= self.eps / 2
+      elif self.method == 'RBF':
+        diff_u0 = torch.pow(torch.reshape(diff_u0, (-1, self.h)),
+                            2) / self.sigma ** 2
+        diff_v0 = torch.pow(torch.reshape(diff_v0, (-1, self.h)),
+                            2) / self.sigma ** 2
+        diff_u0 = torch.exp(-diff_u0)  # Radial basis function
+        diff_v0 = torch.exp(-diff_v0)
+      elif self.method == 'inverse-quadratic':
+        diff_u0 = torch.pow(torch.reshape(diff_u0, (-1, self.h)),
+                            2) / self.sigma ** 2
+        diff_v0 = torch.pow(torch.reshape(diff_v0, (-1, self.h)),
+                            2) / self.sigma ** 2
+        diff_u0 = 1 / (1 + diff_u0)  # Inverse quadratic
+        diff_v0 = 1 / (1 + diff_v0)
+      else:
+        raise Exception(
+          f'Wrong kernel method. It should be either thresholding, RBF,' 
+          f' inverse-quadratic. But the given value is {self.method}.')
+      diff_u0 = diff_u0.type(torch.float32)
+      diff_v0 = diff_v0.type(torch.float32)
+      a = torch.t(Iy * diff_u0)
+      hists[l, 0, :, :] = torch.mm(a, diff_v0)
 
       Iu1 = torch.unsqueeze(torch.log(I[:, 1] + EPS) - torch.log(I[:, 0] + EPS),
                             dim=1)
       Iv1 = torch.unsqueeze(torch.log(I[:, 1] + EPS) - torch.log(I[:, 2] + EPS),
                             dim=1)
       diff_u1 = abs(
-        Iu1 - torch.unsqueeze(torch.tensor(np.linspace(
-          self.hist_boundary[0], self.hist_boundary[1], num=self.h)),
-          dim=0).to(self.device))
+        Iu1 - torch.unsqueeze(torch.tensor(np.linspace(-3, 3, num=self.h)),
+                              dim=0).to(self.device))
       diff_v1 = abs(
-        Iv1 - torch.unsqueeze(torch.tensor(np.linspace(
-          self.hist_boundary[0], self.hist_boundary[1], num=self.h)),
-          dim=0).to(self.device))
+        Iv1 - torch.unsqueeze(torch.tensor(np.linspace(-3, 3, num=self.h)),
+                              dim=0).to(self.device))
 
       if self.method == 'thresholding':
         diff_u1 = torch.reshape(diff_u1, (-1, self.h)) <= self.eps / 2
@@ -188,45 +146,39 @@ class RGBuvHistBlock(nn.Module):
       diff_u1 = diff_u1.type(torch.float32)
       diff_v1 = diff_v1.type(torch.float32)
       a = torch.t(Iy * diff_u1)
-      if not self.green_only:
-        hists[l, 1, :, :] = torch.mm(a, diff_v1)
-      else:
-        hists[l, 0, :, :] = torch.mm(a, diff_v1)
+      hists[l, 1, :, :] = torch.mm(a, diff_v1)
 
-      if not self.green_only:
-        Iu2 = torch.unsqueeze(torch.log(I[:, 2] + EPS) - torch.log(I[:, 0] +
-                                                                   EPS), dim=1)
-        Iv2 = torch.unsqueeze(torch.log(I[:, 2] + EPS) - torch.log(I[:, 1] +
-                                                                   EPS), dim=1)
-        diff_u2 = abs(
-          Iu2 - torch.unsqueeze(torch.tensor(np.linspace(
-            self.hist_boundary[0], self.hist_boundary[1], num=self.h)),
-            dim=0).to(self.device))
-        diff_v2 = abs(
-          Iv2 - torch.unsqueeze(torch.tensor(np.linspace(
-            self.hist_boundary[0], self.hist_boundary[1], num=self.h)),
-            dim=0).to(self.device))
-        if self.method == 'thresholding':
-          diff_u2 = torch.reshape(diff_u2, (-1, self.h)) <= self.eps / 2
-          diff_v2 = torch.reshape(diff_v2, (-1, self.h)) <= self.eps / 2
-        elif self.method == 'RBF':
-          diff_u2 = torch.pow(torch.reshape(diff_u2, (-1, self.h)),
-                              2) / self.sigma ** 2
-          diff_v2 = torch.pow(torch.reshape(diff_v2, (-1, self.h)),
-                              2) / self.sigma ** 2
-          diff_u2 = torch.exp(-diff_u2)  # Gaussian
-          diff_v2 = torch.exp(-diff_v2)
-        elif self.method == 'inverse-quadratic':
-          diff_u2 = torch.pow(torch.reshape(diff_u2, (-1, self.h)),
-                              2) / self.sigma ** 2
-          diff_v2 = torch.pow(torch.reshape(diff_v2, (-1, self.h)),
-                              2) / self.sigma ** 2
-          diff_u2 = 1 / (1 + diff_u2)  # Inverse quadratic
-          diff_v2 = 1 / (1 + diff_v2)
-        diff_u2 = diff_u2.type(torch.float32)
-        diff_v2 = diff_v2.type(torch.float32)
-        a = torch.t(Iy * diff_u2)
-        hists[l, 2, :, :] = torch.mm(a, diff_v2)
+      Iu2 = torch.unsqueeze(torch.log(I[:, 2] + EPS) - torch.log(I[:, 0] + EPS),
+                            dim=1)
+      Iv2 = torch.unsqueeze(torch.log(I[:, 2] + EPS) - torch.log(I[:, 1] + EPS),
+                            dim=1)
+      diff_u2 = abs(
+        Iu2 - torch.unsqueeze(torch.tensor(np.linspace(-3, 3, num=self.h)),
+                              dim=0).to(self.device))
+      diff_v2 = abs(
+        Iv2 - torch.unsqueeze(torch.tensor(np.linspace(-3, 3, num=self.h)),
+                              dim=0).to(self.device))
+      if self.method == 'thresholding':
+        diff_u2 = torch.reshape(diff_u2, (-1, self.h)) <= self.eps / 2
+        diff_v2 = torch.reshape(diff_v2, (-1, self.h)) <= self.eps / 2
+      elif self.method == 'RBF':
+        diff_u2 = torch.pow(torch.reshape(diff_u2, (-1, self.h)),
+                            2) / self.sigma ** 2
+        diff_v2 = torch.pow(torch.reshape(diff_v2, (-1, self.h)),
+                            2) / self.sigma ** 2
+        diff_u2 = torch.exp(-diff_u2)  # Gaussian
+        diff_v2 = torch.exp(-diff_v2)
+      elif self.method == 'inverse-quadratic':
+        diff_u2 = torch.pow(torch.reshape(diff_u2, (-1, self.h)),
+                            2) / self.sigma ** 2
+        diff_v2 = torch.pow(torch.reshape(diff_v2, (-1, self.h)),
+                            2) / self.sigma ** 2
+        diff_u2 = 1 / (1 + diff_u2)  # Inverse quadratic
+        diff_v2 = 1 / (1 + diff_v2)
+      diff_u2 = diff_u2.type(torch.float32)
+      diff_v2 = diff_v2.type(torch.float32)
+      a = torch.t(Iy * diff_u2)
+      hists[l, 2, :, :] = torch.mm(a, diff_v2)
 
     # normalization
     hists_normalized = hists / (
