a58a6b5138b5ba9986e508d683a22e0d5f3356fc
korayerbas <115890098+korayerbas@users.noreply.github.com>
2023-10-04 22:40:12 +0300

    train_model_histogan_loss.py

--- train_model_histogan_loss.py
+++ train_model_histogan_loss.py
@@ -226,8 +226,8 @@ def train_model():
                     print("Epoch %d, mse: %.4f, psnr: %.4f, vgg: %.4f" % (epoch,
                             loss_mse_eval, loss_psnr_eval, loss_vgg_eval))
                 elif level ==2:
-                    print("Epoch %d, mse: %.4f, psnr: %.4f, vgg: %.4f, ms-ssim: %.4f" % (epoch,
-                            loss_mse_eval, loss_psnr_eval, loss_vgg_eval, loss_ssim_eval))
+                    print("Epoch %d, mse: %.4f, psnr: %.4f, vgg: %.4f, ms-ssim: %.4f, hist_loss: %.4f" % (epoch,
+                            loss_mse_eval, loss_psnr_eval, loss_vgg_eval, loss_ssim_eval, loss_histogram_eval))
                 else:
                     print("Epoch %d, mse: %.4f, psnr: %.4f" % (epoch, loss_mse_eval, loss_psnr_eval))
 
